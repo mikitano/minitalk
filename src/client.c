fbc9f4aa586ff5c3947f753a79210d9c1c4bddcc
Magali Midori Kitano <mkitano@c1r3p3.42sp.org.br>
2025-11-16 19:02:37 -0300

    client incomple

--- src/client.c
+++ src/client.c
@@ -6,15 +6,55 @@
 /*   By: mkitano <mkitano@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 11:27:58 by mkitano           #+#    #+#             */
-/*   Updated: 2025/11/15 14:07:52 by mkitano          ###   ########.fr       */
+/*   Updated: 2025/11/16 16:46:35 by mkitano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+/*
+** Global variable g_ack is used to synchronize the client with the server.
+** It is updated inside the signal handler when an ACK is received.
+** the client only sends the next bit after the server's confirmation.
+*/
+static volatile sig_atomic_t	g_ack = 0;
+
+static void	ft_ack_on(int signal)
+{
+	(void)signal;
+	g_ack = 1;
+}
+
+static void	ft_send_bit_to_server(pid_t pid, unsigned char c)
+{
+	(void)pid;
+	(void)c;
+}
+
 int	main(int ac, char **av)
 {
-	ac = 0;
-	*av = "aaa";
+	pid_t				pid;
+	int					i;
+	unsigned char		c;
+	struct sigaction	ssig;
+
+	if (ac != 3)
+		return (1);
+	pid = (pid_t)ft_atoi(av[1]);
+	if (pid <= 0 || kill(pid, 0) == -1)
+		return (1);
+	sigemptyset(&ssig.sa_mask);
+	ssig.sa_flags = 0;
+	ssig.sa_handler = ft_ack_on;
+	if (sigaction(SIGUSR1, &ssig, NULL) == -1)
+		return (1);
+	i = 0;
+	while (1)
+	{
+		c = (unsigned char)av[2][i++];
+		ft_send_bit_to_server(pid, c);
+		if (c == '\0')
+			break ;
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mkitano <mkitano@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 11:27:30 by mkitano           #+#    #+#             */
-/*   Updated: 2025/11/15 17:51:29 by mkitano          ###   ########.fr       */
+/*   Updated: 2025/11/16 16:34:06 by mkitano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,40 @@
 */
 static t_node	g_data;
 
-static void	ft_hander(int sig, siginfo_t *info, void *extra)
+static void	ft_hander(int sig, siginfo_t *info, void *context)
 {
-	(void) extra;
+	(void) context;
 	if (sig == SIGUSR2)
 		g_data.letter |= (1 << g_data.bit);
 	g_data.bit++;
 	if (g_data.bit == 8)
 	{
-		write(1, &g_data, 1);
+		if (g_data.letter == '\0')
+			write (1, "\n", 1);
+		else
+			write(1, &g_data, 1);
 		g_data.bit = 0;
 		g_data.letter = 0;
 	}
 	if (info && info->si_pid > 0)
-		kill(info, SIGUSR1);
+		kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
-	struct sigaction	sig;
+	struct sigaction	ssig;
 
 	g_data.letter = 0;
 	g_data.bit = 0;
-	ft_putstr_fd("SERVER_PID: ", 1);
+	ft_putstr_fd("server ID: ", 1);
 	ft_putnbr_dec(getpid());
 	write(1, "\n", 1);
-	sigemptyset(&sig.sa_mask);
-	sig.sa_sigaction = ft_hander;
-	sig.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &sig, NULL) == -1)
+	sigemptyset(&ssig.sa_mask);
+	ssig.sa_sigaction = ft_hander;
+	ssig.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &ssig, NULL) == -1)
 		return (1);
-	if (sigaction(SIGUSR2, &sig, NULL) == -1)
+	if (sigaction(SIGUSR2, &ssig, NULL) == -1)
 		return (1);
 	while (1)
 		pause();
